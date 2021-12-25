0da457aa66e4ca4d596f8c293cafc47c6aa880f9
Hi_old <hi_old@aliyun.com>
2021-12-25 19:16:41 +0800

    物品数据bean配置

---
+++ src/HttpServer/bean/SpecialItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HioldMod.src.HttpServer.bean
+{
+    class SpecialItem : Item
+    {
+        public string couCurr { get; set; }
+        public string couType { get; set; }
+        public string couPrice { get; set; }
+        public string couCond { get; set; }
+        public string couDate { get; set; }
+        public string count { get; set; }
+    }
+}
