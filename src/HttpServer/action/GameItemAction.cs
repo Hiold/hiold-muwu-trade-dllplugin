cfd33c3ea6271340efe3da71bded5e1dadbf7579
Hi_old <hi_old@aliyun.com>
2021-12-25 23:31:39 +0800

    111

---
+++ src/HttpServer/action/GameItemAction.cs
@@ -0,0 +1,57 @@
+﻿using HioldMod.HttpServer;
+using HioldMod.HttpServer.common;
+using HioldMod.src.HttpServer.common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HioldMod.src.HttpServer.action
+{
+    class GameItemAction
+    {
+        public static void getSystemItem(HttpListenerRequest request, HttpListenerResponse response)
+        {
+            try
+            {
+                Dictionary<string, string> param = ServerUtils.GetParam(request);
+                List<Dictionary<string, object>> items = new List<Dictionary<string, object>>();
+                foreach (ItemClass _item in ItemClass.list)
+                {
+                    if (_item != null)
+                    {
+                        Dictionary<string, object> rs = new Dictionary<string, object>();
+                        string groupInfo = "";
+                        foreach (string temp in _item.Groups)
+                        {
+                            groupInfo += temp + "|";
+                        }
+                        if (groupInfo.Length > 0)
+                        {
+                            groupInfo = groupInfo.Substring(0, groupInfo.Length - 1);
+                        }
+                        //returnResult += string.Format("{0},{1},{2},{3}", _item.Name, _item.CustomIcon, (_item.CustomIconTint.a + "|" + _item.CustomIconTint.r + "|" + _item.CustomIconTint.g + "|" + _item.CustomIconTint.b), groupInfo);
+                        rs.Add("itemname", _item.Name);
+                        rs.Add("icon", _item.CustomIcon);
+                        rs.Add("tint", (_item.CustomIconTint.a + "|" + _item.CustomIconTint.r + "|" + _item.CustomIconTint.g + "|" + _item.CustomIconTint.b));
+                        rs.Add("group", groupInfo);
+                        items.Add(rs);
+                    }
+                }
+
+                ResponseUtils.ResponseSuccessWithData(response, SimpleJson2.SimpleJson2.SerializeObject(items));
+
+
+
+            }
+            catch (Exception e)
+            {
+                LogUtils.Loger(e.Message);
+                LogUtils.Loger(e.StackTrace);
+                LogUtils.Loger(e.Source);
+            }
+        }
+    }
+}
--- src/HttpServer/router/MainRouter.cs
+++ src/HttpServer/router/MainRouter.cs
@@ -1,4 +1,5 @@
 ﻿using HioldMod.src.HttpServer.action;
+using HioldMod.src.HttpServer.common;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,7 +24,25 @@ namespace HioldMod.HttpServer.router
                 url = request.RawUrl;
             }
             //登录
-            if (url.Equals("/api/login")) LoginAction.login(request, response);
+            if (url.Equals("/api/login"))
+            {
+                LoginAction.login(request, response);
+            }
+            else if (url.Equals("/api/getSystemItem"))
+            {
+                GameItemAction.getSystemItem(request, response);
+            }
+            //没有匹配的router 返回404
+            else
+            {
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes("404-ERROR");
+                //对客户端输出相应信息.
+                response.ContentLength64 = buffer.Length;
+                System.IO.Stream output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+                //关闭输出流，释放相应资源
+                output.Close();
+            }
         }
     }
 }
