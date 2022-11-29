c986e91cdfe336837a4301132ee58d059539ad6a
Hi_old <hi_old@aliyun.com>
2022-11-29 11:23:36 +0800

    wys

---
+++ wys_plugin/Class1.cs
@@ -0,0 +1,28 @@
+﻿using HioldMod.src.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wys_plugin
+{
+    public class Class1 : HioldMod.src.Plugins.PluginInterface
+    {
+        public void InitPlugin()
+        {
+            HioldMod.HttpServer.LogUtils.Loger("开始初始化QQBot插件");
+            HioldMod.HttpServer.LogUtils.Loger("已加载QQBot插件");
+        }
+
+        public PluginInfo PluginInfo()
+        {
+            return new HioldMod.src.Plugins.PluginInfo()
+            {
+                Author = "海鸥",
+                PluginName = "QQ机器人插件",
+                Version = "alpha0.1"
+            };
+        }
+    }
+}
---
+++ wys_plugin/action/ManageAction.cs
@@ -0,0 +1,56 @@
+﻿using HioldMod.HttpServer;
+using HioldMod.HttpServer.common;
+using HioldMod.src.HttpServer.attributes;
+using HioldMod.src.HttpServer.bean;
+using HioldMod.src.HttpServer.common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using static HioldMod.HioldMod;
+
+namespace QQ_BOTPlugin.bot.action
+{
+    [ActionAttribute]
+    public class ManageAction
+    {
+        [RequestHandlerAttribute(IsServerReady = true, IsUserLogin = true, IsAdmin = true, url = "/api/setBotConfig")]
+        public static void setBotConfig(HioldRequest request, HttpListenerResponse response)
+        {
+            try
+            {
+                //获取参数
+                string postData = ServerUtils.getPostData(request.request);
+                Dictionary<string, string> queryRequest = (Dictionary<string, string>)SimpleJson2.SimpleJson2.DeserializeObject(postData, typeof(Dictionary<string, string>));
+                queryRequest.TryGetValue("lottery", out string containerid);
+                queryRequest.TryGetValue("qunNumber", out string qunNumber);
+                //
+                string path = string.Format("{0}/plugins/bot.json", HioldMod.HioldMod.API.AssemblyPath);
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                }
+                //实例化一个文件流--->与写入文件相关联
+                FileStream fs = new FileStream(path, FileMode.Create);
+                //实例化BinaryWriter
+                StreamWriter bw = new StreamWriter(fs);
+                bw.Write(SimpleJson2.SimpleJson2.SerializeObject(queryRequest));
+                //清空缓冲区
+                bw.Flush();
+                //关闭流
+                bw.Close();
+                //重新加载配置文件
+                ResponseUtils.ResponseSuccess(response);
+            }
+            catch (Exception e)
+            {
+                LogUtils.Loger(e.Message);
+                ResponseUtils.ResponseFail(response, "参数异常");
+                return;
+            }
+        }
+    }
+}
