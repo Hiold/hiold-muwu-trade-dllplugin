b3e3b34b6433b4a8af7c8f0eed870ee52b9c8995
Hi_old <hi_old@aliyun.com>
2022-12-05 15:43:37 +0800

    update

--- src/API.cs
+++ src/API.cs
@@ -214,6 +214,7 @@ namespace HioldMod
             public bool ChatMessage(ClientInfo _cInfo, EChatType _type, int _senderId, string _msg, string _mainName,
                  bool _localizeMain, List<int> _recipientEntityIds)
             {
+                TradeSysEvents.TrigerChatEvent(_msg, _cInfo);
                 //监听[/sa]命令
                 if (!string.IsNullOrEmpty(_msg) && _msg.EqualsCaseInsensitive("/shop"))
                 {
